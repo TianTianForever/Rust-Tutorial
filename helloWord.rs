47ee2b4bfbb370cf2b74b7395a6213c810fcc854
TianTianForever <2890339972@qq.com>
2016-09-12 18:59:18 +0800

    hello world

---
+++ helloWord.rs
@@ -0,0 +1,12 @@
+use std::{i8,i16,i32,i64,u8,u16,u32,u64,
+    isize,usize,f32,f64};
+
+use std::io::stdin;
+fn main() {
+    println!("hello world!");
+    println!("max i8 {}",i8::MAX);
+    println!("min i8 {}",i8::MIN);
+    println!("MAX i16 {}",i16::MAX);
+    println!("MIN i16 {}",i16::MIN);
+    println!("MAX i32 {}",i32::MAX);
+}
