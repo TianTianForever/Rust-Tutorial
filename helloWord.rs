522713b3ff8dd087ae043c0f8e8d03652cce0b7b
TianTianForever <2890339972@qq.com>
2016-09-15 10:27:06 +0800

    numeric types

--- helloWord.rs
+++ helloWord.rs
@@ -1,12 +1,23 @@
-use std::{i8,i16,i32,i64,u8,u16,u32,u64,
-    isize,usize,f32,f64};
-
-use std::io::stdin;
+use std::{i8,i16,i32,i64,isize,u8,u16,u32,u64,
+    usize,f32,f64};
 fn main() {
-    println!("hello world!");
     println!("max i8 {}",i8::MAX);
     println!("min i8 {}",i8::MIN);
     println!("MAX i16 {}",i16::MAX);
     println!("MIN i16 {}",i16::MIN);
     println!("MAX i32 {}",i32::MAX);
+    println!("MAX i64 {}",i64::MAX);
+    println!("MIN i64 {}",i64::MIN);
+    println!("MAX u8 {}",u8::MAX);
+    println!("MIN u8 {}",u8::MIN);
+    println!("MAX u16 {}",u16::MAX);
+    println!("MIN u16 {}",u16::MIN);
+    println!("MAX u32 {}",u32::MAX);
+    println!("MIN u32 {}",u32::MIN);
+    println!("MAX u64 {}",u64::MAX);
+    println!("MIN u64 {}",u64::MIN);
+    println!("MAX f32 {}",f32::MAX);
+    println!("MIN f32 {}",f32::MIN);
+    println!("MAX f64 {}",f64::MAX);
+    println!("MIN f64 {}",f64::MIN);
 }
