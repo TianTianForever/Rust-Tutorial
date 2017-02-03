3494e6a2016b204719243dec9c869b6c0e9f4252
TianTianForever <2890339972@qq.com>
2017-02-03 20:47:36 +0800

    declare first

--- test24.rs
+++ test24.rs
@@ -1,26 +1,18 @@
 fn main() {
-    // This binding lives in the main function.
-    let long_lived_binding = 1;
-    
-    // This is block, and has a smaller scope than the main function.
+    // Declare a variable binding
+    let a_binding;
     {
-        // This binding only exists in this block.
-        let short_lived_binding = 2;
-        println!(" inner short: {}", short_lived_binding);
-        
-        // This binding 'shadows' the outer one.       
-        let long_lived_binding = 5f32;
-        println!("inner long: {}", long_lived_binding);
+        let x = 2;
+        // Initialize the binding.
+        a_binding = x * x;
     }
-    // End of the block.
-
-    // Error! 'short_lived_binding' doesn't exists in this scope.
-    //println!("Outer short: {}",short_lived_binding);   
-
-    println!("Outer long: {}", long_lived_binding);
-    
-    // This binding also 'shadow' the previous binding.
-    let long_lived_binding = 8u8;
-    println!("Outer long: {}", long_lived_binding);
+    println!("a_binding: {}", a_binding);
+    let another_binding;
+    // Error! Use of uninitialized binding.
+    // The compiler fobids use of unintialized variables, as this would lead to
+    // undefined behavior.
+    // println!("another_binding: {}", another_binding);
+   
+    another_binding = 2;
+    println!("another_binding: {}", another_binding);
 }
-
