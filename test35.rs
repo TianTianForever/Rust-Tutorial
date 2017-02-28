d2d85be4e7418d8b62b4d7b953a3b2565c4d3e80
TianTianForever <2890339972@qq.com>
2017-02-28 12:47:36 +0800

    pointers/ref/mut ref

--- test35.rs
+++ test35.rs
@@ -16,8 +16,34 @@ fn main() {
     match *reference {
         val => println!("Got a value via derefencing: {:?}", val), 
     }
-
+    // What if you don't start with a reference? 'reference' was a '&'
+    // because the right side was already a reference. this is not a reference
+    // because the right side is not one.
     let not_a_reference = 3;
+
+    // Rust provides 'ref' for exactly this purpose. 
+    // It modifies the assignment so that a reference is created for the element;
+    // this reference is assigned.
     let ref is_a_reference = 3;
     
+    // Accordingly, by difining 2 values without references,references
+    // can be retrived via 'ref' and 'ref mut'.
+    let value = 5;
+    let mut mut_value = 6;
+
+    // Use 'ref' keyword to create a reference.
+    match value {
+        ref r => println!("Got a reference to a value: {:?}", r),
+    }
+
+    // Use 'ref mut' similarly.
+    match mut_value {
+        ref mut m => {
+            // Got a reference. Gotta dereference it before we can add anything to it.
+            *m += 10;
+            println!("We added 10, 'mut_value': {:?}", m);       
+        
+        }    
+    }
 }
+
