1afafe63c439fc4ab927ad9ec9bb643319c60df5
TianTianForever <2890339972@qq.com>
2017-03-16 20:37:04 +0800

    use a function of Iterator::any

--- test37.rs
+++ test37.rs
@@ -10,6 +10,11 @@
 fn main() {
     let vec1 = vec![1, 2, 3];
     let vec2 = vec![4, 5, 6];
-    println!("2 in vec1: {:?}",vec1.iter().any(|&x| x == 3));
-    println!("7 in vec2: {:?}",vec2.into_iter().any(|x| x == 7));
+    println!("2 in vec1: {}",vec1.iter().any(|&x| x == 3));
+    println!("7 in vec2: {}",vec2.into_iter().any(|x| x == 7));
+
+    let array1 = [1, 2, 3];
+    let array2 = [4, 5, 6];
+    println!("2 in array: {}", array1.iter().any(|&x| x == 3));
+    println!("7 in array: {}", array2.into_iter().any(|&x| x == 7));
 }
