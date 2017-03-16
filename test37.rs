a3841a18c4be657364d8b571fcfc6b466693dc94
TianTianForever <2890339972@qq.com>
2017-03-16 20:17:07 +0800

    Use a function of Iterator::any

---
+++ test37.rs
@@ -0,0 +1,15 @@
+/// Iterator::any is a function which when passed an iterator, will return 'true' if any element satisfies
+/// the predicate. Otherwise false. its signature:
+///pub trait Iterator {
+///    type Item;
+///    fn any<F>(&mut self, f: F) -> bool where
+///        F: FnMut(Self::Item) -> bool {
+/// 
+///    }
+///}
+fn main() {
+    let vec1 = vec![1, 2, 3];
+    let vec2 = vec![4, 5, 6];
+    println!("2 in vec1: {:?}",vec1.iter().any(|&x| x == 3));
+    println!("7 in vec2: {:?}",vec2.into_iter().any(|x| x == 7));
+}
