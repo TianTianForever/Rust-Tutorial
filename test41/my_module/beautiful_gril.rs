932f018f29591c40b1c02353297442c914d26dc5
matrixlin <tiantiansirius@gmail.com>
2017-03-30 10:34:33 +0800

    modify some files

--- test41/my_module/beautiful_gril.rs
+++ test41/my_module/beautiful_gril.rs
@@ -2,4 +2,11 @@
 pub fn function() {
     println!("called 'my_module::beautiful_gril::function()'");
 }
+fn private_function() {
+    println!("called 'my_module::beautiful_gril::private_function()'");
+}
 
+pub fn indirect_access() {
+    print!("Access 'my_module::beautiful_gril::indirect_access()'");
+    println!("called 'my_module::beautiful::indirect_access()'");
+}
