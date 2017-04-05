404884b621f13cce928e5b08dc147d74975bf27c
matrixlin <tiantiansirius@gmail.com>
2017-04-05 10:45:29 +0800

    exercise traits

---
+++ generics/traits_test1.rs
@@ -0,0 +1,23 @@
+// Non-copyable type.
+struct Empty;
+
+struct Null;
+
+trait DoubleDrop<T> {
+    fn double_drop(self, _: T);
+
+}
+
+impl <T, U> DoubleDrop <T> for U {
+    fn double_drop(self, _: T) {
+
+    }
+}
+
+fn main() {
+    let empty = Empty;
+
+    let null = Null;
+
+    empty.double_drop(null);
+}
