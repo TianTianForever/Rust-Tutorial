bc1e715e40aa8dfd1a4226d45f36ce46161249ac
matrixlin <tiantiansirius@gmail.com>
2017-05-29 17:43:34 +0800

    deleted test11111111.rs

--- test11111111.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    // s is store date on the stack!
-    let s = "hello world!";
-    println!("{}",s);
-    // s2 is store date on the heap!
-    let s2 = String::from("Hello World!");
-    println!("{}",s2);
-}
