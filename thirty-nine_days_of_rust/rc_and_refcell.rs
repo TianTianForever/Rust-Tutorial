b50418a21cab6d9c0372f47e7fef287c9dada09f
TianTianForever <2890339972@qq.com>
2017-07-08 21:07:40 +0800

    The thirty-ninth day of rust

--- thirty-nine_days_of_rust/rc_and_refcell.rs
+++ thirty-nine_days_of_rust/rc_and_refcell.rs
@@ -11,7 +11,6 @@ enum List {
     Empty,
 }
 
-
 struct Value {
     x: Rc<RefCell<i32>>,
     y: Box<i32>,
@@ -24,25 +23,17 @@ impl fmt::Debug for Value {
 }
 
 #[derive(Debug)]
-struct Point {
-    x: Rc<Value>,
-    y: Option<i32>,
-}
+struct Point { x: Rc<Value>, y: Option<i32> }
 
 fn main() {
     let value = Rc::new(RefCell::new(23));
     println!("Reference count : {}", Rc::strong_count(&value));
-
     let a = Cons(value.clone(), Rc::new(Empty));
     println!("Reference count after create a: {}", Rc::strong_count(&value));
-   
-    let share = Rc::new(a);    
-    
+    let share = Rc::new(a);        
     let b = Cons(Rc::new(RefCell::new(24)), share.clone());
     let c = Cons(Rc::new(RefCell::new(25)), share.clone());
-
     *value.borrow_mut() += 1;
-
     println!("Shared after {:?}", share);
     println!("b after {:?}", b);
     println!("c after {:?}", c);
@@ -51,12 +42,10 @@ fn main() {
         x: Rc::new(RefCell::new(1992)),
         y: Box::new(1991),
     };
-   
     let shared_value = Rc::new(number);
     shared_value.clone();
     let point1 = Point {x: shared_value.clone(), y: Some(2) };
     println!("{:?}", point1);
     let point2 = Point {x: shared_value.clone(), y: None };
     println!("{:?}", point2);
-    
 }
