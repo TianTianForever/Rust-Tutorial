f12d46e28911b4f868ddffe541d19a25bd1e353a
TianTianForever <2890339972@qq.com>
2017-07-11 19:33:12 +0800

    The forty-second day of rust

---
+++ forty-one_days_of_rust/forty-one_days_of_rust.rs
@@ -0,0 +1,59 @@
+use std::ops::Mul;
+use std::rc::Rc;
+use std::cell::RefCell;
+use std::fmt;
+
+#[derive(Debug)]
+struct Node {
+    value: i32,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+struct Tree {
+    leaf: i32,
+    branch: RefCell<Vec<Rc<Tree>>>,
+}
+impl fmt::Debug for Tree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Tree {{leaf: {}, branch: {:?}}}", self.leaf, self.branch)
+    }
+}
+
+#[derive(Debug)] struct Point<T> { x: T, y: T }
+
+impl<T: Mul<Output=T>> Mul for Point<T> {
+    type Output = Point<T>;
+    fn mul(self, other: Point<T>) -> Point<T> {
+        Point {
+            x: self.x * other.x,
+            y: self.y * other.y,
+        }
+    }
+}
+
+fn main() {
+    let leaf = Rc::new(
+        Node {
+            value: 5,
+            children: RefCell::new(vec![]),
+        }
+    );
+
+    let branch = Rc::new(
+        Node {
+            value: 5,
+            children: RefCell::new(vec![leaf.clone()]),
+        }
+    );
+    println!("{:?}", Rc::strong_count(&leaf));    
+    println!("{:?}", branch);
+    let tree = Rc::new( Tree {
+        leaf: 10000,
+        branch: RefCell::new(vec![]),
+    });
+    let tree_branch = Rc::new( Tree {
+        leaf: 9999,
+        branch: RefCell::new(vec![tree.clone()]),
+    });
+    println!("{:?}", tree_branch);
+}
---
+++ forty-one_days_of_rust/reference_cycle.rs
@@ -0,0 +1,55 @@
+use std::rc::Rc;
+use std::rc::Weak;
+use std::cell::RefCell;
+use std::fmt;
+
+struct Node {
+    value: i32,
+    parent: RefCell<Weak<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{ Node value: {}, parent: {:?}, children: {:?} }}", self.value, self.parent, self.children)
+    }
+}
+
+#[derive(Debug)]
+struct Tree {
+    value: i32,
+    branch: RefCell<Weak<Tree>>,
+    leaf: RefCell<Vec<Rc<Tree>>>,
+}
+
+fn main() {
+    let leaf = Rc::new( Node {
+        value: 10,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![]),
+    });
+    // Try to get a reference to the parent of leaf by using the 'upgrade' method.
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+    let branch = Rc::new( Node {
+        value: 11,
+        parent: RefCell::new(Weak::new()),
+        children: RefCell::new(vec![leaf.clone()]),
+    });
+    // Using the Rc::downgrade function to create a 'Weak' reference to 'branch' from the 'Rc' in 'branch'.
+    *leaf.parent.borrow_mut() = Rc::downgrade(&branch);
+    println!("leaf parent = {:?}", leaf.parent.borrow().upgrade());
+
+    let tree_leaf = Rc::new( Tree {
+        value: 10,
+        branch: RefCell::new(Weak::new()),
+        leaf: RefCell::new(vec![]),
+    });
+    println!("tree_leaf strong = {:?}, weak = {:?}", Rc::strong_count(&tree_leaf), Rc::weak_count(&tree_leaf));
+    let tree_branch = Rc::new( Tree {
+        value: 11,
+        branch: RefCell::new(Weak::new()),
+        leaf: RefCell::new(vec![tree_leaf.clone()]),
+    });
+    *tree_leaf.branch.borrow_mut() = Rc::downgrade(&tree_branch);        
+    println!("tree_branch strong = {:?}, weak = {:?}", Rc::strong_count(&tree_branch), Rc::weak_count(&tree_branch));
+}
